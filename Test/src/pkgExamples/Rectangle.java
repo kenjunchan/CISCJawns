757e49ed083a97104c2c77dc008fcb33d54a03c4
Ken Chan <Ken Chan@128.4.75.190>
2018-03-15 02:52:37 -0400

    CISCJAWNS3

--- Test/src/pkgExamples/Rectangle.java
+++ Test/src/pkgExamples/Rectangle.java
@@ -3,37 +3,15 @@ package pkgExamples;
 public class Rectangle extends Shape{
 	private double base;
 	private double height;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public Rectangle()
 	{
 		
 	}
-	
 	public Rectangle(double b, double h)
 	{
 		base = b;
 		height = h;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public void baseTimesNumber(int n)
 	{
 		this.base = this.base * n;
